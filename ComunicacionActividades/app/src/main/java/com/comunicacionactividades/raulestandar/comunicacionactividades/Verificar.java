aaa01c289bd0d7a6ecc9cec0e53266a94b001e4d
RaulSierro <raul.sierro@gmail.com>
2015-04-22 13:40:57 +0200

    22/04 a las 13:41

---
+++ ComunicacionActividades/app/src/main/java/com/comunicacionactividades/raulestandar/comunicacionactividades/Verificar.java
@@ -0,0 +1,9 @@
+package com.comunicacionactividades.raulestandar.comunicacionactividades;
+
+import android.app.Activity;
+
+/**
+ * Created by raulinvitado on 22/4/15.
+ */
+public class Verificar extends Activity {
+}
